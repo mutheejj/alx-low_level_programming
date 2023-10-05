a01163376ecb3e4a44823f78ad8ba067087e74f6
john maina <johnmuthee547@gmail.com>
2023-10-05 15:40:10 -0700

    comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
 /*
- *  *john 
- *  *   * muthee
+ *  * file of john
+ *   * done and tired
  *    */
 
-int main(void)
-	/* its void*/
-{
-	/*
-	 *  *for 
-	 *  *   *done
-	 *    */
+#include <stdio.h>
 
+/**
+ *  * okay
+ *   *                new line
+ *    *
+ *     * Always 0 at the end
+ *      */
+int main(void)
+{
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
