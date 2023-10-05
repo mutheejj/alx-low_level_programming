418a7a1df7a7a63917f58db582323a8f77dcd230
john maina <johnmuthee547@gmail.com>
2023-10-05 16:11:36 -0700

    yes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-/*
+/** 
  *  * File: 101-quote.c
  *   * Auth: john maina
  *    */
@@ -6,16 +6,16 @@
 #include <unistd.h>
 
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
+ *  *main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *   *llowed by a new line, to standard error.
  *    *
- *     * Return: Always 1.
+ *     * ReturnAlways 1.
  *      */
 int main(void)
 {
-		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
+	                write(2,
+					                                              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+										                                                            59);
 
-			return (1);
+			                        return (1);
 }
