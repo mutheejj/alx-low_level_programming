bdcea95db4aed8aecdafe86716aca12424abf924
john maina <johnmuthee547@gmail.com>
2023-10-06 14:07:06 -0700

    comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,14 @@
-/*
- *  * File: 5-printf.c
- *   * Auth: john muthee
- *    */
-
-#include <stdio.h>
-
 /**
- *  * main - Prints "with proper grammar, but the outcome is
- *   *                a piece of art," followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ * this is comments for this document
+ * too long
+ * ok
+ */
+#include <stdio.h>
+/**
+ * this is comments for this document
+ * done
+ * done again
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
