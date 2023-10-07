b7a47c28c0148c67cb672cdc99deae0f056f1c92
john maina <johnmuthee547@gmail.com>
2023-10-07 01:39:06 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * file: 2-print_alphabet.c
+ * author: john muthee maina
+ */
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * the program prins alphabets a to z
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for ( alpha = 'a'; alpha <= 'z'; alpha++;)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
