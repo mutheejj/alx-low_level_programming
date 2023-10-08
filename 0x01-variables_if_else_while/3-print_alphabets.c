d69699ee3fe9b12a012a08b6f2b0c785526bab46
john maina <johnmuthee547@gmail.com>
2023-10-07 22:32:47 -0700

    up and low

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low, up;
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+		for (up = 'A'; up <='Z'; upp++;)
+		{
+			putchar(up);
+		}
+		putchar('/n');
+	}
+	return (0);
+}
