07360a3205acf586f01f9acd31310b8714c71d11
john maina <johnmuthee547@gmail.com>
2023-10-07 23:09:36 -0700

    execpt q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'q' && low != 'e')
+		{
+			putchar(low);
+			putchar('\n');
+		}
+	}
+	return (0);
+}
