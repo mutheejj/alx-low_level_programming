1ad3c925a83455b707c19a8eac3c37fe210f2c5b
john maina <johnmuthee547@gmail.com>
2023-10-07 23:20:22 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int base;
+	for (base = 0; base < 10; base++)
+	{
+		printf("%d", base);
+	}
+	printf("\n");
+	return (0);
+}
