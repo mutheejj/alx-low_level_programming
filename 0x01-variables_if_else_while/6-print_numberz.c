5b17ab46bb51b118e6c2acfcc0622f43e9ab9a6a
john maina <johnmuthee547@gmail.com>
2023-10-07 23:38:09 -0700

    done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int num;
 	
-	for (num = 0; num < 10; num++)
+	for (num = 48; num < 58; num++)
 	{
 		putchar(num);
 	}
