54db718710f5980959238091ca200360db6ec1d8
john maina <johnmuthee547@gmail.com>
2023-10-08 00:00:54 -0700

    not sure

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex < 16; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
