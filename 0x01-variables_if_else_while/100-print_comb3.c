ba367ab926f186de2beb0f3705abc45913820fe1
john maina <johnmuthee547@gmail.com>
2023-10-08 01:33:55 -0700

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int three = "3";
+	int nine = "9";
+
+	putchar(three + '0');
+	putchar(',');
+	putchar(' ');
+	putchar(nine + '0');
+	putchar('\n');
+	return (0);
+}
