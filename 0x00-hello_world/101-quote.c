c6902f9e8c77ace0fb037a9422c158ac14180f5e
john maina <johnmuthee547@gmail.com>
2023-10-09 01:43:23 -0700

    doc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,10 @@
-/** 
- *  * File: 101-quote.c
- *   * Auth: john maina
- *    */
-
 #include <unistd.h>
-
 /**
- *  *main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *llowed by a new line, to standard error.
- *    *
- *     * ReturnAlways 1.
- *      */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	                write(2,
-					                                              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-										                                                            59);
-
-			                        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
