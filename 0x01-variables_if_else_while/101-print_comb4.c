cc494b92a9e8b62d9d6160112b3bc0877f6554dd
john maina <johnmuthee547@gmail.com>
2023-10-09 12:29:28 -0700

    i

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,17 +7,17 @@ int main(void)
 {
 	int one, two, three;
 
-	for (one = 0; one < 9; one++)
+	for (one = 0; one < 8; one++)
 	{
 		for (two = one + 1; two < 9; two++)
 		{
-			for (three = two + 1; three < 9; three++)
+			for (three = two + 1; three < 10; three++)
 			{
 				if (one < two && two < three)
 				{
-					putchar(one + '0');
-					putchar(two + '0');
-					putchar(three + '0');
+					putchar((one % 10) + '0');
+					putchar(two % 10) + '0');
+					putchar(three % 10 + '0');
 
 					if (!(one == 7 && two == 8 && three == 9))
 					{
@@ -28,5 +28,6 @@ int main(void)
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
