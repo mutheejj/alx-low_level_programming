51ee05222fa37b801dab47d0d60b3f3c2eb8d062
john maina <johnmuthee547@gmail.com>
2023-10-09 02:17:29 -0700

    git

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,18 @@ int main(void)
 {
 	int nom, num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 9; num++)
 	{
 		for (nom = num + 1; nom < 10; nom++)
 		{
 			putchar((num % 10) + '0');
 			putchar((nom % 10) + '0');
 
-			if (num == 5 && nom == 6)
-				continue;
-
+			if (num == 8 && nom == 9)
+			{
 				putchar(',');
 				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
