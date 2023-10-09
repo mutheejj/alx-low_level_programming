6326e8b21bdfefe2a115f6f2e28729559092f7d3
john maina <johnmuthee547@gmail.com>
2023-10-09 01:32:13 -0700

    docummentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,14 @@
-/*
- *  * File: 6-size.c
- *   * Auth: john maina
- *    */
-
 #include <stdio.h>
-
 /**
- *  * main  Print the size of types based on size
- *   *
- *    * Return 0.
- *     */
+ * main - Entry point
+ * Return Always 0 (Success)
+ */
 int main(void)
 {
-		printf("Size of a char: %zu byte(s)\n", sizeof(char));
-			printf("Size of an int: %zu byte(s)\n", sizeof(int));
-				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
-							return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
