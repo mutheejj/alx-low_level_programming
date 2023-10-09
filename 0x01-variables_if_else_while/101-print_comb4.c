dc8cb3d90117f03ed79529e18f0b2c8de8ab7d42
john maina <johnmuthee547@gmail.com>
2023-10-09 12:20:49 -0700

    add

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 
 	for (one = 0; one < 9; one++)
 	{
-		for (two = 0; two < 8; two++)
+		for (two = 0; two < 9; two++)
 		{
-			for (three = 0; three < 7; three++)
+			for (three = 0; three < 9; three++)
 			{
 				if (one < two && two < three)
 				{
