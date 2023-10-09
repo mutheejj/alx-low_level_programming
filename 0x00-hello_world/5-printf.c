d1a8b785621d95c2ccafd5379b772af6fb7cf00c
john maina <johnmuthee547@gmail.com>
2023-10-09 01:22:55 -0700

    documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-/**
- * print- with grammar but the outcome
- * return - (0)
- */
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
