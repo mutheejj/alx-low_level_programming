18cfad6d05f7d620dac28d27cdf9df34aa19ba32
john maina <johnmuthee547@gmail.com>
2023-10-11 14:01:50 -0700

    l

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs(int) - Entry point
+ * @n: he integer to be computed.
+ * Return: The absolute value of the integer.
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
