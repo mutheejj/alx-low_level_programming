f5b5b5643c5546729454304eb373e84ef3c4e3dd
john maina <johnmuthee547@gmail.com>
2023-10-11 12:27:08 -0700

    line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		for (line = 0; line < 10; line++)
+		{
+			_putchar('\n');
+		}
+		_putchar(alpha);
+	}
+}
