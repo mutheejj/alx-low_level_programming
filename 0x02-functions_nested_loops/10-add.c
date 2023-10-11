6fa5c36bad88198ebfaec4463abd95795684c839
john maina <johnmuthee547@gmail.com>
2023-10-11 15:44:23 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @one: to be added
+ * @two: to be adede
+ * Return: the result
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
+int add(int, int);
 #endif
