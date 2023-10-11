322d9519accf68a75eec44f78f5b21b51c7c309f
john maina <johnmuthee547@gmail.com>
2023-10-11 12:31:56 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 	char alpha;
 	int line;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	for (line = 0; line < 10; line++)
 	{
-		for (line = 0; line < 10; line++)
+		for (alpha = 0; alpha < 10; alpha++)
 		{
-			_putchar('\n');
+			_putchar(alpha);
 		}
-		_putchar(alpha);
+		_putchar('\n');
 	}
 }
