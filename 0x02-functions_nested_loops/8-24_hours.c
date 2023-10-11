769b88587600f1e3edea60a0df6b9200a9dbb92e
john maina <johnmuthee547@gmail.com>
2023-10-11 14:37:22 -0700

    l

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ */
+void jack_bauer(void)
+{
+	int h,m;
+	for (h = 0; h <= 23; h++)
+	{
+		for ( m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			 _putchar((m / 10) + '0');
+			 _putchar((m / 10) + '0');
+			 _putchar((m % 10) + '0');
+		}
+	}
+}
+
