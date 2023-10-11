5ca13fd39cdc5f59872a40f47739f6c8b27aa235
john maina <johnmuthee547@gmail.com>
2023-10-11 13:48:07 -0700

    l

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * print_sign - Entry point
  * @n: character to be tested
  * Return:1 if the number is greater than zero,
- * 	0 if the number is zero,
- * 	-1 if the number is less than zero.
+ *	0 if the number is zero,
+ *	-1 if the number is less than zero.
  */
 int print_sign(int n)
 {
