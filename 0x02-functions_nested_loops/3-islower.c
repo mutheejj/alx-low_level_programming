0f444ff46f0d2d7fc6b9e435a74a15773f7970f5
john maina <johnmuthee547@gmail.com>
2023-10-11 12:59:40 -0700

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	char letter;
+	int n;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
+
