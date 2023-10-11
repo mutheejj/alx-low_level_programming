7101cbe4bd449b39a7cde184e587ec42237bb7e4
john maina <johnmuthee547@gmail.com>
2023-10-11 15:15:54 -0700

    l

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.c"
+/**
+ * times_table - Entry point
+ */
+void times_table(void)
+{
+	int nine, num, times;
+
+	for (nine = 0; nine <= 9; nine++)
+	{
+		_putchar('0');
+		for (num = 1; num <= 9; num++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			times = nine * num;
+
+			if (times <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((times / 10) + '0');
+			}
+			_putchar((times % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
