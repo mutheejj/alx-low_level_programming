0a1a40e447d41b53c1da0b25a5e5d859ce6d7ae6
john maina <johnmuthee547@gmail.com>
2023-10-12 14:01:45 -0700

    num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ */
+void more_numbers(void)
+{
+	int num, line;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num + '0');
+		}
+		_putchar('\n');
+	}
+}
