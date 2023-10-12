fbd20a3b90335da7b70281a13e9e003a40d60588
john maina <johnmuthee547@gmail.com>
2023-10-12 15:30:08 -0700

    l

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,5 @@
 void print_number(int n)
 {
 	int pli;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		pli = -pli;
-	}
-	if ((pli / 10) > 0)
-	{
-		print_number(pli / 10);
-	}
-	_putchar((pli % 10) + '0');
+	_putchar(n + '0');
 }
