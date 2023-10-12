e9f30d80939bd2019b497848a2ef95837f2f3411
john maina <johnmuthee547@gmail.com>
2023-10-12 12:57:14 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: character to be checked
+ * Return: Always 0 (success)
+ */
+int _isupper(int c)
+{
+	if (c = 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this are additional projects on nested loops
