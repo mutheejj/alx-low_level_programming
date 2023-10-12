6f62eb331b0aa9151f2e1521922b4a234ff9ec91
john maina <johnmuthee547@gmail.com>
2023-10-12 13:48:12 -0700

    k

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints numbers
+ */
+void print_most_numbers(void)
+{
+	int num;
+	for (num = 0; num <=9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
