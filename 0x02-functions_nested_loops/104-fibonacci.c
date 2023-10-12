aac90322ca3e47d85109429a08c52409813612a7
john maina <johnmuthee547@gmail.com>
2023-10-12 10:53:05 -0700

    push

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first = 1, second = 2, next, n;
+	
+	printf("%d, %d, ", first, second);
+	for (int i = 0; i <= 98; i++)
+	{
+		next = first + second;
+		printf("%d", next);
+		if (i <= 98)
+		{
+			printf(", ");
+		}
+		first = second;
+		second = next;
+	}
+	printf("\n");
+	return (0);
+}
