d4baf10385321c16efa2963c25cb34abd866eec7
john maina <johnmuthee547@gmail.com>
2023-10-12 14:58:45 -0700

    l

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
  * print_diagonal - Entry point
- * Return: Always 0 (Success)
+ * @n: The number of \ characters to be printed.
  */
 void print_diagonal(int n)
 {
 	int i, line;
 
-	for(i = 0; i < n;  i++)
+	if (n > 0)
 	{
-		_putchar('\');
-		_putchar('\n');
-		for (line = 0; line < n; line ++)
+		for(i = 0; i < n;  i++)
 		{
 			_putchar(' ');
+			_putchar('\\');
+			if (line == n - 1)
+			{
+				_putchar('\n');
+			}
 		}
 	}
-	return (0);
+	_putchar('\n');
 }
