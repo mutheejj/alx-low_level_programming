56685e5d1ed5598210c3d95c7afed990202a9caa
john maina <johnmuthee547@gmail.com>
2023-10-12 13:16:03 -0700

    l

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - entry
+ * Return: 1 if its a digit, else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define main_h
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 #endif
