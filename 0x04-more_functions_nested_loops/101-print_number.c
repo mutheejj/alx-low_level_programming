6c39b28dfa34f4d5e5bb69675ded348ff43f0a52
john maina <johnmuthee547@gmail.com>
2023-10-13 04:18:35 -0700

    l

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,15 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
+	if (n > 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	print_number(n / 10);
 	return (0);
 }
