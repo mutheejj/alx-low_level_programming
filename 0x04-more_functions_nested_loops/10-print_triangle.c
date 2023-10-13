7ce332aa4ea9bef810e8f6dfddebb3c65d574a89
john maina <johnmuthee547@gmail.com>
2023-10-13 03:34:13 -0700

    line

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_triangle - prints a tringle
+ * @size: size of the tringle
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int size)
+{
+	int i;
+
+	for (i = 0; i 
