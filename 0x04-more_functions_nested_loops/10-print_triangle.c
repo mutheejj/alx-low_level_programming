431fa06db1688b0173dcef77f5498b9534eea730
john maina <johnmuthee547@gmail.com>
2023-10-14 03:35:20 -0700

    l

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,6 +6,23 @@
  */
 void print_triangle(int size)
 {
-	int i;
+	int hash, index;
 
-	for (i = 0; i 
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+			{
+				_putchar(' ');
+			}
+			for (index = 0; index < hash; index++)
+			{
+				_putchar('#');
+			}
+			if (hash == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+}
