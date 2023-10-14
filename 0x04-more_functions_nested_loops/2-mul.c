bd1973cdda311910d84ec408bdbd34e8d87a5a85
john maina <johnmuthee547@gmail.com>
2023-10-13 23:16:20 -0700

    simple

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,23 +7,5 @@
  */
 int mul(int a, int b)
 {
-	int mult, lst;
-
-	mult = a * b;
-	if (mult == 0)
-	{
-		_putchar('0');
-	}
-	else if (mult < 0)
-	{
-		_putchar('-');
-		mult = -mult;
-	}
-	while (mult > 0)
-	{
-		lst = mult % 10;
-		putchar(lst + '0');
-		mult /=10;
-	}
-	return (0);
+	return (a * b);
 }
