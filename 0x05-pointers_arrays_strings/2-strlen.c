7027f44eb828cbc7c34f03a2a94d9d1488a374cf
john maina <johnmuthee547@gmail.com>
2023-10-16 08:39:23 -0700

    display teh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - displays the lenth
+ * @s: to be determined
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = _strlen(*s);
+	return (a);
+}
