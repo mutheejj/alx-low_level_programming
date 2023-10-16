bb92f29792d217006958e873994149a5c474ffa5
john maina <johnmuthee547@gmail.com>
2023-10-16 15:05:53 -0700

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverse
+ * @s: to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char a, b;
+
+	a = s;
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	for (k = 1; k < j; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (k / 2); i++)
+	{
+		b = s[i];
+		s[i] = *a;
+		*a = b;
+		a--;
+	}
+}
