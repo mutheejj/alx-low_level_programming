83015bd6f57cd8805ea716b4fd5b9b10e6544a2b
john maina <johnmuthee547@gmail.com>
2023-10-18 08:19:04 -0700

    add h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - hlf the letters
+ * @str: to be printed
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	
+	j = i / 2;
+	if (i % 2 == 1)
+	{
+		j++;
+	}
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
