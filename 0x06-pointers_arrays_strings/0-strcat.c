5886120f31e28abde0e108fbf63a25b5164a38bb
john maina <johnmuthee547@gmail.com>
2023-10-20 19:01:21 -0700

    l

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concentates files
+ * @dest: to be chckes
+ * @src: to be copied
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+in this directory i have all the projects of more of arrays and pointers
