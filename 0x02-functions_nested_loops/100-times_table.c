0a72fc0f9154943db4c3e0f5af80804d0d22ae44
john maina <johnmuthee547@gmail.com>
2023-10-21 14:43:26 -0700

    l

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,3 +1,20 @@
 #include "main.h"
 /**
- *
+ * print_times_table - prints the table of multiplication
+ * @n:to be checked
+ */
+void print_times_table(int n)
+{
+	int i, j, mult = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		for ( j = 0; j < n; j++)
+		{
+			mult = i * j;
+			_putchar(mult + '0');
+		}
+		
+		_putchar('\n');
+	}
+}
