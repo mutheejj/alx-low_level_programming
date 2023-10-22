4749054a5daadfbdda1f0ecad21b6ebcd1dcecfd
john maina <johnmuthee547@gmail.com>
2023-10-21 23:15:55 -0700

    hello

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int i = 0;
+	int j = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[j] != n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
+		i++;
 		j++;
 	}
 	dest[i] = '\n';
