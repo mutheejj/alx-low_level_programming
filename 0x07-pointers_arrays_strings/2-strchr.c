d2f39cd5fe5b0b650e3bf960fd41d2bb601712be
john maina <johnmuthee547@gmail.com>
2023-10-23 05:13:05 -0700

    l

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strchr - locate a character
  * @s: is a string
@@ -18,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
