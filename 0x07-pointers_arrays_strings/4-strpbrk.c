ee5a49109a57c22232a1487f20106fb898ec16a1
john maina <johnmuthee547@gmail.com>
2023-10-23 18:11:04 -0700

    chnged the return value

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: to be checked
  * @accept: to be checked
- * Return: ointer to the byte in s that matches one of the bytes in accept, or NULL
+ * Return: matches one of the bytes in accept, or NULL
  */
 char *_strpbrk(char *s, char *accept)
 {
@@ -15,7 +15,7 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (s[i] == accept[j])
 			{
-				return &(s[i]);
+				return (s[i]);
 			}
 		}
 	}
