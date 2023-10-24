fbcd06da24b97905cdd075d96f53348377709f4e
john maina <johnmuthee547@gmail.com>
2023-10-23 17:35:42 -0700

    i have been abble to copy the address

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copies the memory
+ * @dest: to be copied the memory
+ * @src: to copy the memory
+ * @n: to be cheked
+ * Returns: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
