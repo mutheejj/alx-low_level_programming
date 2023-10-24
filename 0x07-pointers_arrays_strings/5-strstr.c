1fe2c5f4fc4ee0bb157092307e230d0e52f0e81b
john maina <johnmuthee547@gmail.com>
2023-10-23 18:53:33 -0700

    l

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,6 +8,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
@@ -16,9 +17,9 @@ char *_strstr(char *haystack, char *needle)
 				break;
 		}
 		if (needle[j] == '\0')
-			{
-				return &(haystack[i]);
-			}
+		{
+			return &(haystack[i]);
+		}
 	}
 	return ('\0');
 }
