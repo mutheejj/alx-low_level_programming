ca5df7d69debe26036afd4a16589fa3c411efb3d
john maina <johnmuthee547@gmail.com>
2023-10-28 03:41:00 -0700

    success

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string with recursion
+ */
+void _puts_recursion(char *s)
+{
+	while (s[i] != '\0')
+	{
+		_putchar(s);
+		i++;
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+in this directory i have the projects on 0x08. C - Recursion
