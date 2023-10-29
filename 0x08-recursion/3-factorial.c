cc4f98d363ae03c3ec234d066c71a58f59212bcc
john maina <johnmuthee547@gmail.com>
2023-10-28 22:22:30 -0700

    i have made the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial -  returns the factorial of a given number.
+ * @n: is the number to be factorized
+ * Return: 0, -1 or n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
