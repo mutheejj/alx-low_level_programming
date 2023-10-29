2faa8bf385495faaa584241df7ad10b828f4086f
john maina <johnmuthee547@gmail.com>
2023-10-28 23:21:46 -0700

    made a function to check odd numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_prime_number -  returns 1 if the input integer is a
+ * prime number, otherwise return 0
+ * @n: the number to be checked.
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 1)
+		return (0);
+	return (1);
+}
