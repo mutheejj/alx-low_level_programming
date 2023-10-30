930804ce1c89639c38cf8dca70c8195616b1e85d
john maina <johnmuthee547@gmail.com>
2023-10-30 01:40:06 -0700

    not yet

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,8 +8,6 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
@@ -21,13 +19,6 @@ int is_prime_number(int n)
 	if (n % 2 == 0)
 	{
 		return (0);
-	}
-	for (i = 5; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-		{
-			return (0);
-		}
 	}
 	return (1);
 }
