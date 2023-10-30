170e77118d9f48f16498b33c56c574c0332a3bae
john maina <johnmuthee547@gmail.com>
2023-10-30 03:03:40 -0700

    l

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * wildcmp - ompares two strings returns 0 or 1
+ * @s1: a string
+ * @s2: a string
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
