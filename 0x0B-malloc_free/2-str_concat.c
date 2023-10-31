d12e611fe56dcd72ff0811e172d527c89c7c8eb0
john maina <johnmuthee547@gmail.com>
2023-10-31 14:37:06 -0700

    l

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - str_concat
+ * @s1: is a string
+ * @s2: is also a string
+ * Return: null or a string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int n;
+
+	new = malloc (n * sizeof (*s1 + *s2);
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	retrun (new);
+}
+
