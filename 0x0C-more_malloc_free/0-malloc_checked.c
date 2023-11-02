2d131f0ccdfd1985ea40d279ec599168d1066a11
john maina <johnmuthee547@gmail.com>
2023-11-02 15:04:00 -0700

    exit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - is a function
+ * @b: is an iteger
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *there = malloc(b);
+
+	if (there == NULL)
+	{
+		exit(98);
+	}
+	return (there);
+}
