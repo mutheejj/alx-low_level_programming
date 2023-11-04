d8908fcf50a06ccd72ab9c77a674b2df93a27a0e
john maina <johnmuthee547@gmail.com>
2023-11-03 22:44:21 -0700

    betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * main - is the start of the program
+ * @argv: is a string of vecrors
+ * @argc: is an integer
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
