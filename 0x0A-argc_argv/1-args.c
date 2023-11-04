2d5a0dd13101cabeec4fb43ab67c93d75322c1ec
john maina <johnmuthee547@gmail.com>
2023-11-03 22:24:19 -0700

    i have limited the p

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,20 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum = sum + atoi(argv[i]);
-		}
-	}
-	else
-	{
-		printf("not found");
-	}
-	printf("%d\n", sum);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
