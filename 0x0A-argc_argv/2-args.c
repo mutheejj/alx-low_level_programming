184f36a8b82510446bb4e519849acb84195499a5
john maina <johnmuthee547@gmail.com>
2023-11-03 22:41:02 -0700

    its a sucess

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int main(int argc, char **argv)
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
