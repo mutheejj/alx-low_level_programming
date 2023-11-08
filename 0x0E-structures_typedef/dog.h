c4159e2327836aaaab93fc4b7bfd875b2b06e77b
john maina <johnmuthee547@gmail.com>
2023-11-08 03:46:20 -0800

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - is a structure
+ * @name: is a name
+ * @age: is a float
+ * @owner: is a char
+ */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 #endif
