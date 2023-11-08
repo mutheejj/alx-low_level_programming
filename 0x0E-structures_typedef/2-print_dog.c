bdbd393254aa69cbfee66901b55012b9c94dc20d
john maina <johnmuthee547@gmail.com>
2023-11-08 05:16:25 -0800

    6 float

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %.000001f\n", d->age);
+	printf("Age: %6f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
