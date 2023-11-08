8414dc3d294b21271b4993da8be55c8e1c3993ad
john maina <johnmuthee547@gmail.com>
2023-11-08 14:33:56 -0800

    done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - is a function
+ * @array: is an array
+ * @cmp: is a pointe
+ * Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+				{
+					return (i);
+				}
+				i++;
+			}
+		}
+	}
+	return (-1);
+}
