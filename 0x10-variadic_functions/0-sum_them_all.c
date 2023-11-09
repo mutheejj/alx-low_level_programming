19dff7ffc5c013d2c39865fa1a15dff8628c2901
john maina <johnmuthee547@gmail.com>
2023-11-09 12:00:22 -0800

    betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	int sum = 0;
 	unsigned int i;
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_start (args, n);
-	
+	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
