ffb0c55d98f8bb13cbe184fcf4b760041a41fa46
john maina <johnmuthee547@gmail.com>
2023-11-16 11:48:23 -0800

    betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,12 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length
+ * @next: next code
+ */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
