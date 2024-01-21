e955a716a9e7b206b0f808141c452ce189361b55
mutheejj <johnmuthee547@gmail.com>
2024-01-21 00:01:22 -0800

    b

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,5 +7,38 @@
  */
 void print_buffer(char *b, int size)
 {
+    int i, j;
 
+    if (size <= 0)
+    {
+        printf("\n");
+        return;
+    }
+
+    for (i = 0; i < size; i += 10)
+    {
+        printf("%08x: ", i);
+
+        for (j = i; j < i + 10; j++)
+	{
+		if (j < size)
+                printf("%02x", (unsigned char)b[j]);
+            else
+                printf("  ");
+
+            if (j % 2 != 0)
+                printf(" "); // Add a space after every two bytes
+        }
+
+        // Print the content of the buffer, replacing non-printable characters with '.'
+        for (j = i; j < i + 10 && j < size; j++)
+        {
+            if (b[j] >= 32 && b[j] <= 126)
+                printf("%c", b[j]);
+            else
+                printf(".");
+        }
+
+        printf("\n");
+    }
 }
